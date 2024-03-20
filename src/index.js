12ad2228f24abf40385fc8e9e85f5b5575619a04
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2024-03-21 02:21:39 +0900

    stateを試した

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import EventBasic from "./EventBasic";
+import StateBasic from "./StateBasic";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <EventBasic type='time' />
+    <StateBasic init={1} />
   </React.StrictMode>
 );
 
