a9660879bdca07de66fb1bf615206fc9191e38ff
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2024-03-21 02:25:38 +0900

    commit漏れ

---
+++ src/StateBasic.js
@@ -0,0 +1,14 @@
+import {useState} from "react";
+
+export default function StateBasic({init}){
+  const [count, setCount] = useState(init);
+  console.log(`count is ${count}`)
+
+  const handleClick = () => setCount(count + 1)
+  return (
+      <>
+        <button onClick={handleClick}>カウント</button>
+        <p>{count}回、クリックされました</p>
+      </>
+  )
+}
