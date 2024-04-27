c1943f809d6bc150fcbaa13a23c015671e9bfa8e
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2024-04-28 01:59:13 +0900

    Interactionsタブを試した

--- src/stories/MyButton.stories.js
+++ src/stories/MyButton.stories.js
@@ -1,5 +1,7 @@
 import MyButton from "./MyButton";
 import {action} from '@storybook/addon-actions';
+import {within, userEvent, fn} from "@storybook/test";
+import {expect} from "@storybook/jest";
 
 export default {
     title: 'MyApp/MyButton',
@@ -30,7 +32,15 @@ export const Index = {
         size: 'medium',
         handleClick: e => {
             action('clicked')(e, new Date())
-        }
+        },
+        onClick: fn()
+    },
+    play: async ({args, canvasElement}) => {
+        const canvas = within(canvasElement)
+        const button = canvas.getByRole('button')
+        await userEvent.click(button)
+        await userEvent.click(button)
+        await expect(args.onClick).toHaveBeenCalledTimes(2)
     }
 }
 
